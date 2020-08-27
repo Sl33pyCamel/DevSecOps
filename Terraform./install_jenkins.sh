0a9544b75dc9a731cb3f72317bdb54e2e33d4d50
Sl33pyCamel <54852786+Sl33pyCamel@users.noreply.github.com>
2020-08-27 10:03:53 -0400

    Update install_jenkins.sh
    Configure Jenkins needs to be analyzed further for situtation.

--- Terraform./install_jenkins.sh
+++ Terraform./install_jenkins.sh
@@ -29,3 +29,16 @@ sudo chkconfig jenkins on
 sudo service docker start
 sudo service jenkins start
 
+echo "Configure Jenkins"
+mkdir -p /var/lib/jenkins/init.groovy.d
+mv /tmp/basic-security.groovy /var/lib/jenkins/init.groovy.d/basic-security.groovy
+mv /tmp/disable-cli.groovy /var/lib/jenkins/init.groovy.d/disable-cli.groovy
+mv /tmp/csrf-protection.groovy /var/lib/jenkins/init.groovy.d/csrf-protection.groovy
+mv /tmp/disable-jnlp.groovy /var/lib/jenkins/init.groovy.d/disable-jnlp.groovy
+mv /tmp/jenkins.install.UpgradeWizard.state /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+mv /tmp/node-agent.groovy /var/lib/jenkins/init.groovy.d/node-agent.groovy
+chown -R jenkins:jenkins /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+mv /tmp/jenkins /etc/sysconfig/jenkins
+chmod +x /tmp/install-plugins.sh
+bash /tmp/install-plugins.sh
+service jenkins start
